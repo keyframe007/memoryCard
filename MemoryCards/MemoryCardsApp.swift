33f62b003d576c71eacfefb6c8f7a5c6be950edc
keyframe007 <keyframe007@gmail.com>
2023-10-09 10:14:32 -0400

    Initial Commit

---
+++ MemoryCards/MemoryCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryCardsApp.swift
+//  MemoryCards
+//
+//  Created by Ali Tezel on 2023-10-09.
+//
+
+import SwiftUI
+
+@main
+struct MemoryCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
