3dc6c29b25757783f12456f30306abc5f1a3112c
keyframe007 <keyframe007@gmail.com>
2023-10-10 16:07:48 -0400

    some button stuff

--- MemoryCards/ContentView.swift
+++ MemoryCards/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var exitCount = 0
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Rectangle()
+                .ignoresSafeArea()
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                
+            Button(action: {
+                exitCount += 1
+            }, label: {
+                ZStack {
+                    
+                    Image(systemName: "rectangle.portrait.and.arrow.right.fill")
+                        .font(.title)
+                        .foregroundColor(.black)
+                        .imageScale(.large)
+                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
+                        .background(.red)
+                        .cornerRadius(10)
+                        
+                    Text("EXIT: \(exitCount)")
+                        .foregroundStyle(.white)
+                        .bold()
+                        .padding(.top, 65.0)
+                    
+                        
+                }
+                
+                .padding()
+                
+            })
         }
-        .padding()
+        
+        
+        
+        
+        
+        
     }
 }
 
