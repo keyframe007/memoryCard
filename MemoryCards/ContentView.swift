85a66f3d1463a940a0c994f9dba29a9055a3497d
keyframe007 <keyframe007@gmail.com>
2023-10-11 21:13:08 -0400

    moved the card to its own struct

--- MemoryCards/ContentView.swift
+++ MemoryCards/ContentView.swift
@@ -9,32 +9,99 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var isUp = true
-    
     
     var body: some View {
-        ZStack{
-            if isUp {
-                Rectangle()
-                     .frame(width: 100)
-                     .foregroundStyle(.white)
-                     .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 2)
-                Text("💀")
-            } else {
-                
-                Rectangle()
-                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
-                    .foregroundStyle(.orange)
-            }
+        HStack {
+            BaseCard()
+            BaseCard()
+            BaseCard()
+            BaseCard()
+            BaseCard()
         }
-        
-        
-        
-        
+        .padding()
         
         
     }
 }
+    
+    
+    
+            
+        
+struct BaseCard: View {
+    
+    @State private var isUp = true
+    
+    var body: some View {
+        Button(action: {
+            isUp.toggle()
+        }, label: {
+            
+            ZStack{
+                if isUp {
+                    Rectangle()
+                         .frame(width: 100)
+                         .foregroundStyle(.white)
+                         .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 2)
+                    Text("💀")
+                } else {
+                    
+                    Rectangle()
+                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                        .foregroundStyle(.orange)
+                }
+            }
+        })
+        
+    }
+}
+
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
